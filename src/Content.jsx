152217ce9793326d313d86d2b6c38eb6f6d3f24a
Kadyr <kadyr.g03@gmail.com>
2024-10-18 22:36:49 +0200

    first commit

---
+++ src/Content.jsx
@@ -0,0 +1,11 @@
+function Content() {
+    return(
+        <main>
+            <section className='intro'>
+                fdfd
+            </section>
+        </main>
+    );
+}
+
+export default Content;
---
+++ src/Header.jsx
@@ -0,0 +1,23 @@
+function Header() {
+    return(
+        <header>
+            <a className="logo" href="#">K</a>
+            <div className="middle">
+                <ul>
+                    <li>
+                        <a href="">Gallery</a>
+                    </li>
+                    <li>
+                        <a href="">About Me</a>
+                    </li>
+                    <li>
+                        <a href="">Skills & Tools</a>
+                    </li>
+                </ul>
+            </div>
+            <a href="#" className="contact">Contact</a>
+        </header>
+    );
+}
+
+export default Header;
