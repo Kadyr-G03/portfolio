0adf11827834964f3915bdbaeababc6c782a5ca0
Kadyr <kadyr.g03@gmail.com>
2024-10-18 23:24:17 +0200

    Update Content.jsx

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,7 +2,7 @@ function Content() {
     return(
         <main>
             <section className='intro'>
-                <img src="./public/kadyr.png" alt="" />
+                <img src="https://www.dsautomobiles.hu/content/dam/ds/master/ds-range/ds-3-midlife/thumbnails/ds3_all_engines.png" alt="" />
             </section>
         </main>
     );
