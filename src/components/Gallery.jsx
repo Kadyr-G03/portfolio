71c34794d8ed9ed13ce2d7d58bfc4f60333d12d1
Kadyr <kadyr.g03@gmail.com>
2024-10-25 01:12:42 +0200

    committed

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -8,16 +8,16 @@ function App() {
                 <a className="pr-[20px] pt-[15px] min-[800px]:pr-[30px]" href="#">see all</a>
             </span>
 
-            <div class="px-[20px] py-[50px]">
+            <div class="px-[30px] py-[50px] max-w-[1200px]">
                 <div class="columns-1 gap-8 space-y-8 min-[700px]:columns-2 min-[1100px]:columns-3">
-                    <img className="break-inside-avoid w-[300px] rounded-[40px] overflow-hidden" src="https://images.squarespace-cdn.com/content/v1/61c4da8eb1b30a201b9669f2/e2e0e62f-0064-4f86-b9d8-5a55cb7110ca/Korembi-January-2024.jpg" class="w-full h-auto object-cover" />
-                    <img className="break-inside-avoid w-[300px] rounded-[20px]" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Waterfall_at_Arthur%27s_Pass_National_Park.jpg/1200px-Waterfall_at_Arthur%27s_Pass_National_Park.jpg" class="w-full h-auto object-cover" />
-                    <img className="break-inside-avoid w-[300px] rounded-[20px]" src="https://www.lombardodier.com/files/live/sites/loportail/files/news/2021/May/20210521/Nature_LOgreen.jpg" class="w-full h-auto object-cover" />
-                    <img className="break-inside-avoid w-[300px] rounded-[20px]" src="https://rukminim2.flixcart.com/image/850/1000/kgsb1jk0-0/poster/n/h/y/medium-twfnp2-beautiful-waterfall-nature-view-large-size-high-original-imafwy37qv2b5g3v.jpeg?q=90&crop=false" class="w-full h-auto object-cover" />
-                    <img className="break-inside-avoid w-[300px] rounded-[20px]" src="https://i0.wp.com/post.healthline.com/wp-content/uploads/2019/06/Female_Woods_hike_1296x728-header-1-1296x728.jpg?w=1155&h=1528" class="w-full h-auto object-cover" />
-                    <img className="break-inside-avoid w-[300px] rounded-[20px]" src="https://iucn.org/sites/default/files/styles/what_we_do_large/public/images-themes/biodiversity-shutterstock_1477256246.jpg.webp?itok=4i9JdtFu" class="w-full h-auto object-cover" />
-                    <img className="break-inside-avoid w-[300px] rounded-[20px]" src="https://www.pbs.org/wnet/nature/files/2019/07/Super-Hummingbirds-2.jpg" class="w-full h-auto object-cover" />
-                    <img className="break-inside-avoid w-[300px] rounded-[20px]" src="https://natureinvestmenthub.ca/wp-content/uploads/2023/10/nathan-dumlao-xjhgZCuvg3k-unsplash-scaled.jpg" class="w-full h-auto object-cover" />
+                    <img className="w-full break-inside-avoid rounded-[20px]" src="https://images.squarespace-cdn.com/content/v1/61c4da8eb1b30a201b9669f2/e2e0e62f-0064-4f86-b9d8-5a55cb7110ca/Korembi-January-2024.jpg" />
+                    <img className="w-full break-inside-avoid rounded-[20px]" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Waterfall_at_Arthur%27s_Pass_National_Park.jpg/1200px-Waterfall_at_Arthur%27s_Pass_National_Park.jpg" />
+                    <img className="w-full break-inside-avoid rounded-[20px]" src="https://www.lombardodier.com/files/live/sites/loportail/files/news/2021/May/20210521/Nature_LOgreen.jpg" />
+                    <img className="w-full break-inside-avoid rounded-[20px]" src="https://rukminim2.flixcart.com/image/850/1000/kgsb1jk0-0/poster/n/h/y/medium-twfnp2-beautiful-waterfall-nature-view-large-size-high-original-imafwy37qv2b5g3v.jpeg?q=90&crop=false" />
+                    <img className="w-full break-inside-avoid rounded-[20px]" src="https://i0.wp.com/post.healthline.com/wp-content/uploads/2019/06/Female_Woods_hike_1296x728-header-1-1296x728.jpg?w=1155&h=1528" />
+                    <img className="w-full break-inside-avoid rounded-[20px]" src="https://iucn.org/sites/default/files/styles/what_we_do_large/public/images-themes/biodiversity-shutterstock_1477256246.jpg.webp?itok=4i9JdtFu" />
+                    <img className="w-full break-inside-avoid rounded-[20px]" src="https://www.pbs.org/wnet/nature/files/2019/07/Super-Hummingbirds-2.jpg" class="w-full h-auto object-cover" />
+                    <img className="w-full break-inside-avoid rounded-[20px]" src="https://rukminim2.flixcart.com/image/850/1000/kgsb1jk0-0/poster/n/h/y/medium-twfnp2-beautiful-waterfall-nature-view-large-size-high-original-imafwy37qv2b5g3v.jpeg?q=90&crop=false" />
                 </div>
             </div>
         </section>
